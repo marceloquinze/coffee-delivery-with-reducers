8325811e7246ccf2731d84fa83fbd12150580960
Marcelo Vieira <marceloquinze@gmail.com>
2024-07-26 11:33:45 -0300

    Added Store Notice message

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,29 +2,50 @@ import { NavLink } from 'react-router-dom'
 import siteLogo from '../../assets/Logo.svg'
 import { HeaderContainer } from './styles'
 import { ShoppingCart, User } from 'phosphor-react'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { ItemsContext } from '../../contexts/ItemsContext'
 
 export function Header() {
   const { itemsInCart } = useContext(ItemsContext)
   const totalItems = itemsInCart.reduce((acc, item) => acc + item.qty, 0)
+
+  const [hasNotice, setHasNotice] = useState(() => {
+    return !localStorage.getItem('hasNotice')
+  })
+
+  function closeNotice() {
+    setHasNotice(false)
+    localStorage.setItem('hasNotice', 'false')
+  }
+
   return (
     <HeaderContainer className="header-container">
-      <nav>
-        <NavLink to="/">
-          <img src={siteLogo} alt="" />
-        </NavLink>
-      </nav>
-      <div className="user-items">
+      <div className={`store-notice ${!hasNotice ? 'hidden' : ''}`}>
+        <p>
+          This is a demo store for testing purposes — no orders shall be
+          fulfilled.
+        </p>
+        <button onClick={closeNotice} className="close">
+          X
+        </button>
+      </div>
+      <div className="wrapper">
         <nav>
-          <NavLink to="/user" className="location">
-            <User size={24} weight="fill" />
-          </NavLink>
-          <NavLink to="/checkout" className="cart">
-            <ShoppingCart size={24} weight="fill" />
-            {totalItems !== 0 && <span>{totalItems}</span>}
+          <NavLink to="/">
+            <img src={siteLogo} alt="" />
           </NavLink>
         </nav>
+        <div className="user-items">
+          <nav>
+            <NavLink to="/user" className="location">
+              <User size={24} weight="fill" />
+            </NavLink>
+            <NavLink to="/checkout" className="cart">
+              <ShoppingCart size={24} weight="fill" />
+              {totalItems !== 0 && <span>{totalItems}</span>}
+            </NavLink>
+          </nav>
+        </div>
       </div>
     </HeaderContainer>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,57 +1,84 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
-  max-width: 70rem;
-  margin: 0 auto;
-  padding: 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  .store-notice {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem 2rem;
+    background-color: ${(props) => props.theme['yellow-light']};
+    position: relative;
+    z-index: 1;
 
-  .user-items {
-    nav {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 0.75rem;
+    &.hidden {
+      display: none;
     }
 
-    .location {
-      background-color: ${(props) => props.theme['purple-light']};
-      text-decoration: none;
-      display: flex;
-      align-items: center;
-      gap: 0.75rem;
-      padding: 0.5rem;
-      border-radius: 5px;
-      height: 40px;
+    .close {
+      position: absolute;
+      right: 1rem;
+      top: 1rem;
+      cursor: pointer;
+      border: none;
+      background-color: transparent;
+      color: ${(props) => props.theme['yellow-dark']};
+      font-weight: bold;
+      font-size: 120%;
     }
+  }
+  .wrapper {
+    max-width: 70rem;
+    margin: 0 auto;
+    padding: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-    .cart {
-      background-color: ${(props) => props.theme['yellow-light']};
-      display: inline-block;
-      padding: 0.5rem;
-      border-radius: 5px;
-      height: 40px;
-      position: relative;
-
-      span {
-        position: absolute;
-        right: -7px;
-        top: -7px;
-        height: 19px;
-        width: 19px;
-        background-color: ${(props) => props.theme['yellow-dark']};
-        border-radius: 50%;
-        color: ${(props) => props.theme.white};
-        font-size: 0.75rem;
+    .user-items {
+      nav {
         display: flex;
         align-items: center;
         justify-content: center;
+        gap: 0.75rem;
       }
 
-      svg {
-        fill: ${(props) => props.theme['yellow-dark']};
+      .location {
+        background-color: ${(props) => props.theme['purple-light']};
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        gap: 0.75rem;
+        padding: 0.5rem;
+        border-radius: 5px;
+        height: 40px;
+      }
+
+      .cart {
+        background-color: ${(props) => props.theme['yellow-light']};
+        display: inline-block;
+        padding: 0.5rem;
+        border-radius: 5px;
+        height: 40px;
+        position: relative;
+
+        span {
+          position: absolute;
+          right: -7px;
+          top: -7px;
+          height: 19px;
+          width: 19px;
+          background-color: ${(props) => props.theme['yellow-dark']};
+          border-radius: 50%;
+          color: ${(props) => props.theme.white};
+          font-size: 0.75rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        svg {
+          fill: ${(props) => props.theme['yellow-dark']};
+        }
       }
     }
   }
